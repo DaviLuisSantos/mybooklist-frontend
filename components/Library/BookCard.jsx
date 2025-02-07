5024cc63a997ffccecf54dc74639cee2b474bc34
Davi Luis <costasantos.daviluis@gmail.com>
2025-02-07 17:47:21 -0300

    Book Card Responsive

--- components/Library/BookCard.jsx
+++ components/Library/BookCard.jsx
@@ -49,16 +49,17 @@ const BookCard = ({
     return (
         <>
             <div
-                className="rounded-lg shadow-md overflow-hidden flex sm:flex-col md:flex-row lg:block cursor-pointer transform transition-transform duration-200 hover:scale-105 border border-gray-200 lg:h-80"
+                className="rounded-lg shadow-md overflow-hidden flex sm:flex-row md:flex-row lg:flex-col cursor-pointer transform transition-transform duration-200 hover:scale-105 border border-gray-200 lg:min-h-80 sm:min-h-0"
                 onClick={handleModalOpen}
             >
                 {/* Capa do Livro */}
-                <div className="relative w-1/3 h-36 sm:w-full sm:h-48 md:h-full lg:h-48 lg:w-full flex-shrink-0">
+                <div className="relative w-2/5 h-auto sm:w-2/5 sm:h-auto md:w-1/3 md:h-full lg:w-full lg:h-48 flex-shrink-0">
                     <Image
                         src={cover}
                         alt={`Capa do livro ${title}`}
                         layout="fill"
                         objectFit="cover"
+                        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                     />
                     <div
                         className={`absolute top-2 left-2 text-xs font-medium px-2 py-1 rounded-full ${getStatusColor(status)} ${getStatusTextColor(status)}`}
@@ -68,16 +69,14 @@ const BookCard = ({
                 </div>
 
                 {/* Informações do Livro */}
-                <div className="flex flex-col flex-grow p-4 text-left sm:text-center gap-3 relative md:h-full lg:items-center lg:justify-center">
-                    <h2 className="text-lg font-bold md:text-xl lg:text-center" id='book-title'>{title}</h2>
-                    <p className=" mt-1 sm:mt-2 md:text-center" id='book-author'>{author}</p>
-                    <div className="absolute bottom-2 left-2 flex justify-start sm:justify-center flex-wrap items-center lg:static lg:mt-2">
-                        <span className="inline-block px-2 py-1 rounded-full text-xs font-medium text-[#939393]" id='date'>
+                <div className="flex flex-col flex-grow p-4 text-left sm:text-left gap-3 relative md:h-full lg:items-center lg:justify-center">
+                    <h2 className="text-lg font-bold md:text-xl lg:text-center" id="book-title">{title}</h2>
+                    <h2 className="mt-1 sm:mt-2 md:text-center" id="book-author">{author}</h2>
+                    <div className="flex justify-between w-full mt-2 lg:mt-2 lg:mb-2 sm:justify-between md:justify-between">
+                        <span className="inline-block px-2 py-1 rounded-full text-xs font-medium text-[#939393]" id="date">
                             {startDate}
                         </span>
-                    </div>
-                    <div className="absolute bottom-2 right-2 flex justify-start sm:justify-center flex-wrap items-center lg:static">
-                        <span className="inline-block px-2 py-1 rounded-full text-xs font-medium text-white" id='genre'>
+                        <span className="inline-block px-2 py-1 rounded-full text-xs font-medium text-white" id="genre">
                             {genre}
                         </span>
                     </div>
