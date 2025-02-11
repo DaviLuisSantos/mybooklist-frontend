6fc059ee2fdcfebefa44a0da3c1e2125dab4cd63
Davi Luis <costasantos.daviluis@gmail.com>
2025-02-11 00:06:37 -0300

    google login button

--- components/User/Login.jsx
+++ components/User/Login.jsx
@@ -3,6 +3,9 @@ import { useRouter } from 'next/router';
 import { UserContext } from '../../contexts/UserContext';
 import CreateAccount from './CreateAccount';
 import ForgotPassword from './ForgotPassword';
+import { useGoogleLogin, GoogleLogin } from '@react-oauth/google';
+import { FcGoogle } from "react-icons/fc";
+import ThemeChange from '../ThemeChange';
 
 const Login = () => {
     const { login, loading, error, user } = useContext(UserContext);
@@ -29,6 +32,9 @@ const Login = () => {
             console.error('Erro ao fazer login:', error);
         }
     };
+    const loginG = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+    })
 
     return (
         <div className="min-h-screen flex">
@@ -37,7 +43,7 @@ const Login = () => {
 
             {/* Formulário de login */}
             <div className="w-full lg:w-2/5 flex items-start justify-center ">
-
+            <ThemeChange />
                 <div className="px-4 py-6 w-full lg:w-4/5">
                     <div className="flex items-center justify-center mb-8">
                         <svg className="w-8 h-8 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -49,7 +55,6 @@ const Login = () => {
                     <h2 className="text-2xl font-semibold text-left my-4">
                         Nice to see you again
                     </h2>
-
                     <form onSubmit={handleSubmit}>
                         <div className="py-2">
                             <input
@@ -126,21 +131,12 @@ const Login = () => {
                     <div className="mt-6">
                         <button
                             className="flex items-center justify-center w-full px-4 py-3 rounded-lg border border-gray-300 hover:bg-gray-100 focus:outline-none text-sm lg:text-base"
-                            id="button">
-                            <svg className="w-5 h-5 mr-2" viewBox="0 0 48 48">
-                                <defs>
-                                    <path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.7 6.3 29.1 4 24 4c-11.8 0-21.3 9.5-21.3 21.3 0 11.8 9.5 21.3 21.3 21.3 11.8 0 21.3-9.5 21.3-21.3 0-.9-0.1-1.7-0.2-2.5z" />
-                                </defs>
-                                <clipPath id="b">
-                                    <use xlinkHref="#a" overflow="visible" />
-                                </clipPath>
-                                <path className="QvGUp" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.7 6.3 29.1 4 24 4c-11.8 0-21.3 9.5-21.3 21.3 0 11.8 9.5 21.3 21.3 21.3 11.8 0 21.3-9.5 21.3-21.3 0-.9-0.1-1.7-0.2-2.5z" fill="#4285F4" />
-                                <path className=" মুদ্রিত" d="M21.5 44.6c5.7 0 10.6-2 14.3-5.6l-6.4-6.4c-2.2 1.5-5.1 2.5-7.9 2.5-4.8 0-8.8-3.2-10.2-6.8l-11.9 1C6.5 36.2 12.2 44.6 21.5 44.6z" fill="#34A853" />
-                                <path className="YjxLc" d="M37.1 12.9L37.1 12.9c-2.2-1.5-5.1-2.5-7.9-2.5-4.8 0-8.8 3.2-10.2 6.8l11.9-1C41.3 14.8 35.6 6.4 26.2 6.4c-5.7 0-10.6 2-14.3 5.6l6.4 6.4C18 11.3 20.8 10.3 23.7 10.3c4.8 0 8.8 3.2 10.2 6.8L37.1 12.9z" fill="#FBBC05" />
-                                <path className="WrvBxc" d="M24 44C10.2 44 2 34.7 2 24S10.2 4 24 4c11 0 21 8.2 21 18.3-1.3 4.1-4.7 7.2-8.9 9.4l-6.1 6.1c2.1 2.2 3.5 5.3 3.5 8.6z" fill="#EA4335" />
-                            </svg>
+                            id="button"
+                            onClick={() => loginG()}>
+                            <FcGoogle className='text-2xl'/>
                             Or sign in with Google
                         </button>
+
                     </div>
 
                     <div className="mt-6 text-center">
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,18 +1,22 @@
 import { UserProvider } from '@/contexts/UserContext';
 import { BooksProvider } from '../contexts/UserBookContext';
 import { ThemeProvider } from 'next-themes';
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
     return (
-        <UserProvider>
-            <BooksProvider>
-                <ThemeProvider attribute="class" defaultTheme='dark'>
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </BooksProvider>
-        </UserProvider>
+        <GoogleOAuthProvider clientId='1087239602529-9bv30fm57pnto3vitrofk8ihcjl6iaiq.apps.googleusercontent.com'>
+            <UserProvider>
+                <BooksProvider>
+                    <ThemeProvider attribute="class" defaultTheme='dark'>
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </BooksProvider>
+            </UserProvider>
+        </GoogleOAuthProvider>
+
     );
 }
 
